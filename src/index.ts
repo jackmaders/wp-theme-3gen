1d19a60ff096f210139a4bef8338098f1dbbcba3
Jack William Maders <42032268+jackmaders@users.noreply.github.com>
2024-03-04 13:33:48 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import "./styles/main.css";
+import main from "./scripts/main.ts";
+
+main();
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{ts,js}", "./theme/**/*.{php,html}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-full-bleed"),
+    require("@tailwindcss/typography"),
+  ],
+};
