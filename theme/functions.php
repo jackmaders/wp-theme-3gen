2b068ed3540c1beaa922b79fee52ce0b11e3273e
Jack Maders <jackwmaders@gmail.com>
2024-03-05 19:57:41 +0000

    add dynamic hero content

--- theme/functions.php
+++ theme/functions.php
@@ -38,8 +38,8 @@ function threeGen_meta_viewport()
 
 function threeGen_customize_register($wp_customize)
 {
-    $wp_customize->add_section("threeGen_hero_image", [
-        "title" => __("Hero Image", "threeGen"),
+    $wp_customize->add_section("threeGen_hero", [
+        "title" => __("Hero", "threeGen"),
         "priority" => 120,
     ]);
 
@@ -51,10 +51,32 @@ function threeGen_customize_register($wp_customize)
     $wp_customize->add_control(
         new WP_Customize_Image_Control($wp_customize, "hero_image", [
             "label" => __("Upload Hero Image", "threeGen"),
-            "section" => "threeGen_hero_image",
+            "section" => "threeGen_hero",
             "settings" => "hero_image",
         ]),
     );
+    
+    $wp_customize->add_setting('hero_heading', [
+        'default' => 'Default text',
+    ]);
+
+    $wp_customize->add_control('hero_heading', [
+        'label' => 'Hero Text',
+        'section' => 'threeGen_hero',
+        'settings' => 'hero_heading',
+        'type' => 'text',
+    ]);
+
+    $wp_customize->add_setting('hero_subheading', [
+        'default' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur nisl dolor, in porttitor augue luctus eget. Quisque vulputate elementum.',
+    ]);
+    
+    $wp_customize->add_control('hero_subheading', [
+        'label' => 'Hero Subheading',
+        'section' => 'threeGen_hero',
+        'settings' => 'hero_subheading',
+        'type' => 'textarea',
+    ]);
 }
 
 add_action("init", "threeGen_theme_support");
--- theme/templates-parts/hero.php
+++ theme/templates-parts/hero.php
@@ -1,5 +1,9 @@
 <!-- prettier-ignore -->
-<?php $hero_image = get_theme_mod("hero_image"); ?>
+<?php 
+$hero_image = get_theme_mod("hero_image"); 
+$hero_heading = get_theme_mod("hero_heading");
+$hero_subheading = get_theme_mod("hero_subheading");
+?>
 
 <style>
   header::before {
@@ -11,13 +15,9 @@
 <header
   class="container relative flex flex-col items-center justify-center gap-6 pb-16 pt-32 text-light before:absolute before:top-0 before:-z-10 before:block before:h-full before:w-screen before:bg-oxford-950/60 before:bg-no-repeat before:bg-blend-multiply before:content-empty lg:flex-row lg:items-start lg:gap-10"
 >
-  <div class="flex flex-1 flex-col gap-2 md:py-4">
-    <h1>Solar, Simplified</h1>
-    <p class="text-2xl">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-      efficitur nisl dolor, in porttitor augue luctus eget. Quisque vulputate
-      elementum.
-    </p>
+  <div class="flex w-full flex-1 flex-col gap-2 md:py-4">
+    <h1><?= $hero_heading ?></h1>
+    <p class="text-2xl"><?= $hero_subheading ?></p>
   </div>
   <div class="flex-1">
     <!-- prettier-ignore -->
